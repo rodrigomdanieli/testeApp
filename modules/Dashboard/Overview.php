b058e7b20dceabcafd3fadf779b60aa328f881ad
rodrigomdanieli <47753921+rodrigomdanieli@users.noreply.github.com>
2020-10-26 10:57:07 -0300

    add Alert Overview Graphic

---
+++ modules/Dashboard/Overview.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace DBModules\Dashboard;
+
+use DBSnoop\Annotations\Auth;
+use DBSnoop\Annotations\Needed;
+use DBSnoop\Annotations\Request;
+use DBSnoop\Annotations\Route;
+use DBSnoop\Annotations\Type;
+use DBSnoop\Entity\User;
+use DBSnoop\Extension\Graphic;
+use DBSnoop\Extension\Server;
+use DBSnoop\System\Response;
+use DBSnoop\System\ServerRequestControl;
+
+class Overview extends ServerRequestControl
+{
+
+    /**
+     *
+     * @Route("/overview/get_alert_graphic")
+     * @Auth("true")
+     *
+     */
+    public function alertGraphic(): Response\JSON
+    {
+
+        $data = Server::getAlertsGraphic(new User($this->SESSION['user_id']));
+        return new Response\JSON($data['status'], $data['data']);
+        
+
+    }
+
+}
