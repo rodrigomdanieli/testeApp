6005a888b24f838d612e17a2318ae870447d244b
rodrigomdanieli <47753921+rodrigomdanieli@users.noreply.github.com>
2020-10-29 11:07:15 -0300

    Update modules User.php

--- modules/User/Users.php
+++ modules/User/Users.php
@@ -8,6 +8,7 @@ use DBSnoop\Annotations\Request;
 use DBSnoop\Annotations\Route;
 use DBSnoop\Annotations\Type;
 use DBSnoop\Entity\User;
+use DBSnoop\Extension\User as ExtensionUser;
 use DBSnoop\System\Response;
 use DBSnoop\System\ServerRequestControl;
 
@@ -25,7 +26,6 @@ class Users extends ServerRequestControl
      */
     public function get_user_info(): Response\JSON
     {
-        
 
         try {
 
@@ -38,5 +38,32 @@ class Users extends ServerRequestControl
             return new Response\JSON("ok", $th->getMessage());
         }
 
+    }
+    /*
+     * @Route("/user/create_first_password")
+     * @Auth("false")
+     * @Type("JSON")
+     * @Request("POST")
+     * @Needed({
+     * "access_hash",
+     * "new_password"
+     * })
+     **/
+    public function create_first_password(): Response\JSON
+    {
+        $hash = $this->REQUEST['access_hash'];
+        $new_password = $this->REQUEST['new_password'];
+        $ext = new ExtensionUser();
+        try {
+            if ($ext->changeFirstPassword($hash, $new_password)) {
+                return new Response\JSON("ok", "INVALID_USER");
+            }
+            return new Response\JSON("ok", "ok");
+        } catch (\Exception $th) {
+            //var_dump($th->getPrevious());
+            return new Response\JSON("error", $th->getMessage());
+        } catch (\Throwable $th) {
+            return new Response\JSON("ok", $th->getMessage());
+        }
     }
 }
