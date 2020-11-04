8938b05816bbbc023959213c9eeb635d12baad49
HTI <rodolfo.bispo@hti.com.br>
2020-11-04 07:59:56 -0600

    Função para validar e reenviar o e-mail

--- modules/Auth/Register.php
+++ modules/Auth/Register.php
@@ -13,6 +13,8 @@ use DBSnoop\System\Response;
 use DBSnoop\System\ServerRequestControl;
 use DBSnoop\Extension\Customer as ExtensionCustomer;
 use DBSnoop\Controller\Email;
+use React\Dns\Resolver\Resolver;
+use DBSnoop\Extension\User as EUser;
 
 class Register extends ServerRequestControl
 {
@@ -136,6 +138,79 @@ class Register extends ServerRequestControl
             return new Response\JSON("error", $th->getMessage());
         } catch (\Throwable $th) {
             return new Response\JSON("ok", $th->getMessage());
+        }
+
+    }
+
+
+    /**
+     *
+     * @Route("/auth/reupper_register")
+     * @Auth(false)
+     * @Type("JSON")
+     * @Request("POST")
+     * @Needed({
+     * "email"
+     * })
+     */
+    public function reupper_register(): Response\JSON
+    {
+
+        // $value_extension = ExtensionCustomer::reupperTempRegisterCache($this->REQUEST['email']);
+        $value_extension = EUser::loadUserByEmail($this->REQUEST['email']);
+
+        if($value_extension){
+            $email = ExtensionCustomer::reupperTempRegisterCache($value_extension);
+        }else{
+            return new Response\JSON("error", "INVALID_EMAIL");
+        }
+
+        try {
+            
+            return new Response\JSON("ok", $email['access_hash']);
+        } catch (\Exception $th) {
+            //var_dump($th->getPrevious());
+            return new Response\JSON("error", $th->getMessage());
+        } catch (\Throwable $th) {
+            return new Response\JSON("error", $th->getMessage());
+        }
+
+    }
+
+
+
+    /**
+     *
+     * @Route("/auth/resend_validation_code")
+     * @Auth(false)
+     * @Type("JSON")
+     * @Request("POST")
+     * @Needed({
+     * "access_hash"
+     * })
+     */
+    public function resend_validation_code(): Response\JSON
+    {   
+        try {
+            $value_extension = ExtensionCustomer::getTempRegisterInformation($this->REQUEST['access_hash']);
+            $get_value = $value_extension['user'];
+            $email = new Email();
+
+            if($value_extension['status'] != 'error'){
+                $email->addAddress($get_value->getEmail(), $get_value->getName());
+                $email->Subject = "Cadastro DBSnoop";
+                $email->Body = "Código de validação DBSnoop: " . $value_extension['validate_code'];
+                $email->Send();
+                return new Response\JSON("ok", "ok");
+            }else{
+                return new Response\JSON("error", "INVALID_ACCESS_HASH");
+            }
+        } catch (\Exception $th) {
+            return new Response\JSON("error", $th->getMessage());
+        }catch(\PHPMailer\PHPMailer\Exception $e){
+            return new Response\JSON("error", $e->getMessage());
+        } catch (\Throwable $th) {
+            return new Response\JSON("error", $th->getMessage());
         }
 
     }
