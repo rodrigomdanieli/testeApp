b93a48b31194a0ee2d621fa6a1186ff961135834
HTI <rodolfo.bispo@hti.com.br>
2020-11-05 09:51:08 -0600

    Crud de Manutenção e Listagem de Alerta

--- modules/Customer/Customer.php
+++ modules/Customer/Customer.php
@@ -2,6 +2,7 @@
 
 namespace DBModules\Customer;
 
+use DBModules\Bot\Maintenance;
 use DBSnoop\Annotations\Auth;
 use DBSnoop\Annotations\Needed;
 use DBSnoop\Annotations\Request;
@@ -13,130 +14,298 @@ use DBSnoop\System\Response;
 use DBSnoop\System\ServerRequestControl;
 use DBSnoop\Extension\Customer as ExtensionCustomer;
 use DBSnoop\Controller\Email;
+use DBSnoop\Entity\Alert;
+use DBSnoop\Entity\Maintenance as EntityMaintenance;
+use DBSnoop\Entity\Server as EntityServer;
+use DBSnoop\Lists\Alert as ListsAlert;
+use DBSnoop\Extension\Maintenance as ExtensionMaintenance;
 
 class Customer extends ServerRequestControl
 {
     /**
      *
-     * @Route("/customer/temp_register")
-     * @Auth(false)
+     * @Route("/customer/new")
+     * @Auth(true)
      * @Type("JSON")
      * @Request("POST")
      * @Needed({
-     * "user_name",
-     * "customer_name",
-     * "email",
-     * "company_code",
-     * "country",
-     * "phone",
-     * "language"
+     * "server",
+     * "start",
+     * "end",
+     * "recurrent",
+     * "reason",
+     * "alert",
+     * "schema"
      * })
      */
-    public function temp_register(): Response\JSON
+    public function new_maintenance(): Response\JSON
     {
+        $array_recurrent = array('No', 'Daily', 'Weekly', 'Monthly', 'Yearly');
+        if(empty($this->REQUEST['server'])){
+            return new Response\JSON("error", "INVALID_SERVER");
+        }
         
-
-        try {
-        
-        $email = new Email();
-        $user = new User();
-        $user->name = $this->REQUEST['user_name'];
-        $user->email = $this->REQUEST['email'];
-        $user->telephone = $this->REQUEST['phone'];
-        $user->preferred_language = $this->REQUEST['language'];
-
-        $customer = new ECustomer();
-        $customer->name = $this->REQUEST['customer_name'];
-        $customer->company_code = $this->REQUEST['company_code'];
-        $customer->country = $this->REQUEST['country'];
-        $valid_extension_customer = ExtensionCustomer::createTemporaryCustomer($customer,$user);
-
-        if(!$valid_extension_customer){
-            return new Response\JSON("error", "ERROR_CREAT_USER");
+        if(!is_numeric($this->REQUEST['server'])){
+            return new Response\JSON("error", "INVALID_SERVER");
         }
 
-        $email->addAddress($this->REQUEST['email'], $this->REQUEST['user_name']);
+        if(empty($this->REQUEST['start'])){
+            return new Response\JSON("error", "EMPTY_FIELD");
+        }
+
+        if(empty($this->REQUEST['recurrent'])){
+            return new Response\JSON("error", "EMPTY_FIELD");
+        }
+
+        if(empty($this->REQUEST['reason'])){
+            return new Response\JSON("error", "EMPTY_FIELD");
+        }
+
+        if(!in_array($this->REQUEST['recurrent'], $array_recurrent)){
+            return new Response\JSON("error", "INVALID_RECURRENT");
+        }
+
+        if(!is_numeric($this->REQUEST['alert'])){
+            return new Response\JSON("error", "INVALID_ALERT");
+        }
+
+        try {
+            
+            $user = new User($this->SESSION['user_id']);
+            $maintenance = new EntityMaintenance($user);
+            $maintenance->server = new EntityServer($this->REQUEST['server'], $user);
+            $maintenance->start = $this->REQUEST['start'];
+            $maintenance->end = $this->REQUEST['end'];
+            $maintenance->recurrent = $this->REQUEST['recurrent'];
+            $maintenance->reason = $this->REQUEST['reason'];
+            $maintenance->alert = $this->REQUEST['alert'];
+            $maintenance->schema = $this->REQUEST['schema'];
+            var_dump($maintenance->save());
+
+            return new Response\JSON("ok", "ok");
+        } catch (\Throwable $th) {
+            return new Response\JSON("error", $th->getMessage());
+        }
+
+    }
+
+    /**
+     *
+     * @Route("/customer/update")
+     * @Auth(true)
+     * @Type("JSON")
+     * @Request("POST")
+     * @Needed({
+     * "maintenance",
+     * "field",
+     * "value"
+     * })
+     */
+    public function update_maintenance(): Response\JSON
+    {
+       $array_field = array("reason");
+       $field = $this->REQUEST['field'];
+       if(!in_array($field, $array_field)){
+            return new Response\JSON("error", "FIELD_WITH_INVALID_VALUE");
+       }
+
+        try {
+            
+            $user = new User($this->SESSION['user_id']);
+            $maintenance = new EntityMaintenance($user, $this->REQUEST['maintenance']);
+            $maintenance->$field = $this->REQUEST['value'];
+
+            
+            var_dump($maintenance->save());
+
+            return new Response\JSON("ok", "ok");
+        } catch (\Throwable $th) {
+            return new Response\JSON("error", $th->getMessage());
+        }
+
+    }
+
+
+
+    /**
+     *
+     * @Route("/customer/get")
+     * @Auth(true)
+     * @Type("JSON")
+     * @Request("POST")
+     * @Needed({
+     * "maintenance"
+     * })
+     */
+    public function get_maintenance(): Response\JSON
+    {
+        $field_maintenance = $this->REQUEST['maintenance']; 
+        if(!is_numeric($field_maintenance)){
+            return new Response\JSON("error", "INVALID_MAINTENANCE");
+        }
+
+        try {
+            $user = new User($this->SESSION['user_id']);
+            $maintenance = new EntityMaintenance($field_maintenance,$user);
+
+            return new Response\JSON("ok", $maintenance->toArray());
+        } catch (\Throwable $th) {
+            return new Response\JSON("error", $th->getMessage());
+        }
+
+    }
+
+    /////-------------------------Acima está tudo certo e testado-----------------------------------------
+
+
+    /////-------------------------TESTAR ESSA FUNÇÃO NOVAMENTE!!!!!!!-----------------------------------------
+    /**
+     *
+     * @Route("/customer/approve")
+     * @Auth(true)
+     * @Type("JSON")
+     * @Request("POST")
+     * @Needed({
+     * "maintenance"
+     * })
+     */
+    public function approve_maintenance(): Response\JSON
+    {
+        $field_maintenance = $this->REQUEST['maintenance'];
+        if(empty($field_maintenance)){
+            return new Response\JSON("error", "EMPTY_FIELD");
+        }
+
+        if(!is_numeric($field_maintenance)){
+            return new Response\JSON("error", "INVALID_MAINTENANCE");
+        }
+        try {
+            $user = new User($this->SESSION['user_id']);
+            $maintenance = new EntityMaintenance($field_maintenance, $user);
+            $extension_maintenance = new ExtensionMaintenance($maintenance);
+            $valid_maintenance = $extension_maintenance->approveMaintenance($maintenance);
+
+            var_dump($valid_maintenance);
+
+            return new Response\JSON("ok", "ok");
+        } catch (\Throwable $th) {
+            return new Response\JSON("error", $th->getMessage());
+        }
+
+    }
+
+    /**
+     *
+     * @Route("/customer/remove")
+     * @Auth(true)
+     * @Type("JSON")
+     * @Request("POST")
+     * @Needed({
+     * "maintenance"
+     * })
+     */
+    public function remove_maintenance(): Response\JSON
+    {
+        $field_maintenance = $this->REQUEST['maintenance'];
+        if(empty($field_maintenance)){
+            return new Response\JSON("error", "EMPTY_FIELD");
+        }
+
+        if(!is_numeric($field_maintenance)){
+            return new Response\JSON("error", "INVALID_MAINTENANCE");
+        }
+        try {
+            $user = new User($this->SESSION['user_id']);
+            $maintenance = new EntityMaintenance($field_maintenance, $user);
+            $extension_maintenance = new ExtensionMaintenance($maintenance);
+            $valid_maintenance = $extension_maintenance->removeMaintenance($maintenance);
+
+            var_dump($valid_maintenance);
+
+            return new Response\JSON("ok", "ok");
+        } catch (\Throwable $th) {
+            return new Response\JSON("error", $th->getMessage());
+        }
+
+    }
+
+
+    /**
+     *
+     * @Route("/customer/remove")
+     * @Auth(true)
+     * @Type("JSON")
+     * @Request("POST")
+     * @Needed({
+     * "maintenance"
+     * })
+     */
+    public function finish_maintenance(): Response\JSON
+    {
+        $field_maintenance = $this->REQUEST['maintenance'];
+        if(empty($field_maintenance)){
+            return new Response\JSON("error", "EMPTY_FIELD");
+        }
+
+        if(!is_numeric($field_maintenance)){
+            return new Response\JSON("error", "INVALID_MAINTENANCE");
+        }
+        try {
+            $user = new User($this->SESSION['user_id']);
+            $maintenance = new EntityMaintenance($field_maintenance, $user);
+            $extension_maintenance = new ExtensionMaintenance($maintenance);
+            $valid_maintenance = $extension_maintenance->finishMaintenance($maintenance);
+
+            var_dump($valid_maintenance);
+
+            return new Response\JSON("ok", "ok");
+        } catch (\Throwable $th) {
+            return new Response\JSON("error", $th->getMessage());
+        }
+
+    }
+
+
+
+    /////-------------------------TESTAR ESSA FUNÇÃO ACIMA NOVAMENTE!!!!!!!-----------------------------------------
+
     
-        $email->Subject = "Cadastro DBSnoop";
 
-        $email->Body = "Código de validação DBSnoop: " . $valid_extension_customer['validate_code'];
 
-        $email->Send();
-        
-        //var_dump(ExtensionCustomer::createTemporaryCustomer($customer,$user)['validate_code']);
 
-            return new Response\JSON("ok", $valid_extension_customer['access_hash']);
-        } catch (\Exception $th) {
-            //var_dump($th->getPrevious());
-            return new Response\JSON("error", $th->getMessage());
-        }catch(\PHPMailer\PHPMailer\Exception $e){
-            return new Response\JSON("error", $e->getMessage());
-        }catch(\Exception $e){
-            return new Response\JSON("error", $e->getMessage());
-        } catch (\Throwable $th) {
-            return new Response\JSON("ok", $th->getMessage());
-        }
-
-    }
+    //--------------------------------------------PARTE DO ALERTA-----------------------------------------
 
 
     /**
      *
-     * @Route("/customer/valid_temp_register")
-     * @Auth(false)
+     * @Route("/customer/list_by_schema")
+     * @Auth(true)
      * @Type("JSON")
      * @Request("POST")
      * @Needed({
-     * "validate_code",
-     * "access_hash"
+     * "schema"
      * })
      */
-    public function valid_temp_register(): Response\JSON
+    public function list_by_schema(): Response\JSON
     {
-        
+        $field_schema = $this->REQUEST['schema'];
+
+        if(empty($field_schema)){
+            return new Response\JSON("error", "EMPTY_FIELD");
+        }
 
         try {
-            $valid = ExtensionCustomer::validateHashTempCustomer($this->REQUEST['access_hash'], $this->REQUEST['validate_code']);
-            if($valid['status'] != 'ok'){
-                return new Response\JSON("error", $valid['msg']);
-            }
-
-            return new Response\JSON("ok", "ok");
-        } catch (\Exception $th) {
-            //var_dump($th->getPrevious());
-            return new Response\JSON("error", $th->getMessage());
+            $filter_schema = array(
+                "schema"  => $field_schema,
+            );
+            $list = new ListsAlert($filter_schema);
+            
+            
+            return new Response\JSON("ok", $list->toArray());
         } catch (\Throwable $th) {
-            return new Response\JSON("ok", $th->getMessage());
+            return new Response\JSON("error", $th->getMessage());
         }
 
     }
-
-    /**
-     *
-     * @Route("/customer/register_subscription")
-     * @Auth(false)
-     * @Type("JSON")
-     * @Request("POST")
-     * @Needed({
-     * "access_hash",
-     * "subscription_id"
-     * })
-     */
-    public function register_subscription(): Response\JSON
-    {
-        try {
-            $valid_access_hash = ExtensionCustomer::registerSubscriptionCustomer($this->REQUEST['access_hash'], $this->REQUEST['subscription_id']);
-            if($valid_access_hash['status'] === 'error'){
-                return new Response\JSON("error", $valid_access_hash['msg']);
-            }
-
-            return new Response\JSON("ok", "ok");
-        } catch (\Exception $th) {
-            //var_dump($th->getPrevious());
-            return new Response\JSON("error", $th->getMessage());
-        } catch (\Throwable $th) {
-            return new Response\JSON("ok", $th->getMessage());
-        }
-
-    }
+    
 }
