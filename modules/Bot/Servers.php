6d77552454c784ca0e11263ad63ca6561949b97e
rodrigomdanieli <47753921+rodrigomdanieli@users.noreply.github.com>
2020-11-17 17:29:50 -0300

    attbot

--- modules/Bot/Servers.php
+++ modules/Bot/Servers.php
@@ -26,7 +26,7 @@ class Servers extends ServerRequestControl
     {
         $filter = array(
             'user' => $this->SESSION['user_id'],
-            'removed' => false
+            'removed' => false,
         );
 
         $servers = new Server($filter);
@@ -45,7 +45,7 @@ class Servers extends ServerRequestControl
     public function listServersStatus(): Response\JSON
     {
         $filter = array(
-            'user' => $this->SESSION['user_id']
+            'user' => $this->SESSION['user_id'],
         );
 
         $servers = new ServersStatus($filter);
@@ -65,29 +65,38 @@ class Servers extends ServerRequestControl
 
         $filter = array(
             'user' => $this->SESSION['user_id'],
-            'removed' => false
+            'removed' => false,
         );
 
         $servers = new Server($filter);
         $cache = new Cache;
-        
+
         $format_1 = new \DateInterval('P1D');
         $format_2 = new \DateInterval('PT1M');
         $final_date = new \DateTime(date("Y-m-d H:i"));
         $servers_data = array();
-        foreach($servers->toArray() as $server){
+        function calcDiffMinutes(\DateTime $date1, \DateTime $date2): int
+        {
+            $diff = $date1->diff($date2);
+            $minutes = $diff->days * 24 * 60;
+            $minutes += $diff->h * 60;
+            $minutes += $diff->i;
+            return $minutes;
+
+        }
+        foreach ($servers->toArray() as $server) {
             $start_date = clone $final_date;
 
             $start_date->sub($format_1);
 
             $servers_data[$server['server_id']] = array();
-            
-            while ($this->calcDiffMinutes($final_date, $start_date) > 0) {
-                
+
+            while (calcDiffMinutes($final_date, $start_date) > 0) {
+
                 $format_date = $start_date->format('Y-m-d H:i');
                 $key = "status_server-" . $server['server_id'] . "_" . $format_date;
                 $data = $cache->get($key);
-                if($data){
+                if ($data) {
                     array_push($servers_data[$server['server_id']], $data);
                 }
             }
@@ -97,13 +106,4 @@ class Servers extends ServerRequestControl
 
     }
 
-    private function calcDiffMinutes(\DateTime $date1, \DateTime $date2)
-    {
-        $diff = $date1->diff($date2);
-        $minutes = $diff->days * 24 * 60;
-        $minutes += $diff->h * 60;
-        $minutes += $diff->i;
-        return $minutes;
-
-    }
 }
