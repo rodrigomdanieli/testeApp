c024f4cba4db5a6dced14abec8c6ae1959e04701
rodrigomdanieli <47753921+rodrigomdanieli@users.noreply.github.com>
2020-11-18 10:59:01 -0300

    teste bot history

--- routines/Bots/Status.php
+++ routines/Bots/Status.php
@@ -47,7 +47,6 @@ class Status
             $format_date = $final_date->format('Y-m-d H:i');
             echo 'Start - ' . $format_date . PHP_EOL;
             $for2 = $ex->getAllActiveServers();
-
             $chunks = array_chunk($for2, 150);
 
             foreach ($chunks as $chunk) {
@@ -59,17 +58,12 @@ class Status
                     } elseif ($pid) {
                         $childPids[] = $pid;
                     } else {
-
-                        $start_date = clone $final_date;
-
-                        $start_date->sub($format_1);
-
                         $cache2 = new CacheRoutines;
 
                         $s = new \DBSnoop\Entity\Server($server['server_id']);
                         $ext_server = new \DBSnoop\Extension\Server($s);
-                        $server_status = array();
-                        $key = "status_history_server-" . $server['server_id'] . "-" . $format_date;
+
+                        $key_cache = sha1("status_history_server-" . $server['server_id'] . "-" . $format_date);
                         
                         $result = $ext_server->getHistory($final_date);
 
@@ -101,43 +95,38 @@ class Status
                                 }
                             }
                         }
-                        $cache2->set($key, $result_to_cache, 24 * 60 * 60);
-                        // while ($this->calcDiffMinutes($final_date, $start_date) > 0) {
-                        //     echo $this->calcDiffMinutes($final_date, $start_date) . PHP_EOL;
-                        //     $format_date = $start_date->format('Y-m-d H:i');
-                        //     $result = $ext_server->getHistory($start_date);
-                        //     $result_to_cache = array(
-                        //         "status" => 'G',
-                        //         "alerts" => array(),
-                        //     );
-                        //     if ($result['status'] == 'ok') {
-                        //         $result = $result['data'];
 
-                        //         if (!empty($result)) {
-                        //             foreach ($result as $key => $val) {
-                        //                 if ($val['status'] == 'R') {
-                        //                     $result_to_cache['status'] = 'R';
-                        //                 } else if ($val['status'] == 'Y' && $result_to_cache['status'] != 'R') {
-                        //                     $result_to_cache['status'] = 'Y';
-                        //                 } else if ($val['status'] == 'B' && $result_to_cache['status'] != 'R' && $result_to_cache['status'] != 'Y') {
-                        //                     $result_to_cache['status'] = 'B';
-                        //                 }
-                        //                 if (!empty($val['alerts'])) {
-                        //                     if (!key_exists($val['service'], $result_to_cache['alerts'])) {
-                        //                         $result_to_cache['alerts'][$val['service']] = array();
-                        //                     }
-                        //                     if (!key_exists($val['type'], $result_to_cache['alerts'][$val['service']])) {
-                        //                         $result_to_cache['alerts'][$val['service']][$val['type']] = array();
-                        //                     }
-                        //                     array_push($result_to_cache['alerts'][$val['service']][$val['type']], $val['alerts']);
-                        //                 }
-                        //             }
-                        //         }
-                        //     }
-                        //     $server_status[$format_date] = $result_to_cache;
-                        //     $start_date->add($format_2);
-                        // }
-                        // $cache2->set($key, $server_status, 15000);
+                        $cache2->set($key_cache, $result_to_cache, 24 * 60 * 60 * 1000);
+
+                        $start_date = clone $final_date;
+
+                        $start_date->sub($format_1);
+                        $temp_data = array();
+        
+                        while ($this->calcDiffMinutes($final_date, $start_date) >= 0) {
+                            $format_date2 = $start_date->format('Y-m-d H:i');
+                            $key_cache2 = sha1("status_history_server-" . $server['server_id'] . "-" . $format_date2);
+                            $in_cache = $cache2->get($key_cache2);
+                            if(!empty($in_cache)){
+                                $temp_data[$format_date2] = $in_cache;
+                            }
+                            if($this->calcDiffMinutes($final_date, $start_date) == 0){
+                                break;
+                            }else{
+                                $start_date->add($format_2);
+                            }
+                            
+                        }
+                        unset($start_date);
+        
+                        if (!empty($temp_data)) {
+                            $cache2->set("last_day_status" . $server['server_id'], $temp_data, 1800);
+                        }
+                        
+                        
+                        unset($cache2);
+                        unset($ext_server);
+                        unset($s);
 
                         exit();
                     }
@@ -156,6 +145,38 @@ class Status
                 }
 
             }
+
+            // foreach ($for2 as $server) {
+
+
+            //     $start_date = clone $final_date;
+
+            //     $start_date->sub($format_1);
+            //     $temp_data = array();
+
+            //     while ($this->calcDiffMinutes($final_date, $start_date) >= 0) {
+            //         $format_date = $start_date->format('Y-m-d H:i');
+            //         $key_cache = sha1("status_history_server-" . $server['server_id'] . "-" . $format_date);
+            //         $in_cache = $cache->get($key_cache);
+            //         if(!empty($in_cache)){
+            //             $temp_data[$format_date] = $in_cache;
+            //             $cache->destroy($key_cache);
+            //         }
+            //         if($this->calcDiffMinutes($final_date, $start_date) == 0){
+            //             break;
+            //         }else{
+            //             $start_date->add($format_2);
+            //         }
+                    
+            //     }
+            //     unset($start_date);
+
+            //     if (!empty($temp_data)) {
+            //         $cache->set("last_day_status" . $server['server_id'], $temp_data, 1800);
+            //     }
+            //     unset($temp_data);
+
+            // }
 
             $end = $this->microtime_float();
 
