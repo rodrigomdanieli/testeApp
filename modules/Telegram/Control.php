a6b005c928d12e79649b7a8098735bf131da8089
rodrigomdanieli <47753921+rodrigomdanieli@users.noreply.github.com>
2020-11-24 15:19:05 -0300

    Up telegram

---
+++ modules/Telegram/Control.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace DBModules\Telegram;
+
+use DBSnoop\Annotations\Auth;
+use DBSnoop\Annotations\Needed;
+use DBSnoop\Annotations\Request;
+use DBSnoop\Annotations\Route;
+use DBSnoop\Annotations\Type;
+use DBSnoop\System\Cache;
+use DBSnoop\System\Response;
+use DBSnoop\System\ServerRequestControl;
+use DBSnoop\System\Utils;
+
+class Control extends ServerRequestControl
+{
+    /**
+     * @Route("/telegram/hook")
+     * @Auth(false)
+     * @Request("GET")
+     * @Needed({
+     *  "update_id",
+     *  "message"
+     * })
+     */
+    public function hook()
+    {
+
+        $response = '';
+
+        var_dump($this->REQUEST['message']);
+
+        
+
+
+        return new Response\JSON('ok', $response);
+    }
+
+    private static function processHelp(\DBSnoop\Entity\User $user, $message){
+
+    }
+
+}
--- routines/Telegram/Start.php
+++ routines/Telegram/Start.php
@@ -2,9 +2,52 @@
 
 namespace DBRoutines\Telegram;
 
-use DBSnoop\Annotations\{Active, Interval};
+use DBSnoop\Annotations\Active;
+use DBSnoop\Annotations\Cron;
+use DBSnoop\Annotations\Interval;
+use DBSnoop\Annotations\StartRunning;
+use DBSnoop\Controller\Telegram;
 
 
 class Start{
+
+
+    /**
+     * @Active
+     * @StartRunning
+     */
+    public function configureTelegram()
+    {
+
+        $commands = array(
+            array(
+                "command" => "help",
+                "description" => "Get help about how to use the bot"
+            ),
+            array(
+                "command" => "registerinfo",
+                "description" => "Check your account registration info"
+            ),
+            array(
+                "command" => "get_servers_error",
+                "description" => "Get all alerts and errors found"
+            ),
+            array(
+                "command" => "get_servers_alert",
+                "description" => "Get all alerts found"
+            ),
+            array(
+                "command" => "get_status_server",
+                "description" => "Get a status report from all servers"
+            )
+        );
+
+        Telegram::setCommands($commands);
+        
+        Telegram::sendToTelegram("/setWebhook", array(
+            "url" => "https://vikings.hti.com.br/api/telegram/hook"
+        ));
+
+    }
 
 }
