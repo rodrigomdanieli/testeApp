46dbc04c73be3f5c48907a641d07156d745b1a6c
HTI <rodolfo.bispo@hti.com.br>
2020-11-27 13:33:07 -0600

    Telegram

--- routines/Telegram/Hook.php
+++ routines/Telegram/Hook.php
@@ -2,6 +2,7 @@
 
 namespace DBRoutines\Telegram;
 
+use DBSnoop\Controller\Lang;
 use DBSnoop\Annotations\Active;
 use DBSnoop\Annotations\Cron;
 use DBSnoop\Annotations\Interval;
@@ -17,25 +18,14 @@ class Hook{
     
     private $available_methods;
 
+    private $available_commands;
+    
     function __construct()
     {
         $this->available_methods = get_class_methods($this);
-    }
-
-    /**
-     * @ Active
-     * @StartRunning
-     */
-    public function configureTelegram()
-    {
-
-        $commands = array(
+        $this->available_commands =  array(
             array(
                 "command" => "help",
-                "description" => "Get help about how to use the bot"
-            ),
-            array(
-                "command" => "cache",
                 "description" => "Get help about how to use the bot"
             ),
             array(
@@ -43,25 +33,40 @@ class Hook{
                 "description" => "Check your account registration info"
             ),
             array(
+                //FEITO
                 "command" => "get_servers_error",
                 "description" => "Get all alerts and errors found"
             ),
             array(
+                //FEITO
                 "command" => "get_servers_alert",
                 "description" => "Get all alerts found"
             ),
             array(
-                "command" => "get_status_server",
+                "command" => "get_all_status",
+                "description" => "Get a status report from all servers"
+            ),
+            array(
+                "command" => "get_status_maintenance",
                 "description" => "Get a status report from all servers"
             ),
         );
 
-        Telegram::setCommands($commands);
+    }
+
+    /**
+     * @Active
+     * @StartRunning
+     */
+    public function configureTelegram()
+    {
+
+        Telegram::setCommands($this->available_commands);
         
     }
 
     /**
-     * @ Active
+     * @Active
      * @Interval(1)
      */
     public function hook()
@@ -94,8 +99,8 @@ class Hook{
                         $response = self::$method($user, $messages);
                         if(is_array($response)){
                             foreach($response as $msg){
-                                echo "MESSAGE" . PHP_EOL;
-                                echo $msg . PHP_EOL;
+                                // echo "MESSAGE" . PHP_EOL;
+                                // echo $msg . PHP_EOL;
                                 Telegram::sendMessage($chat , $msg);
                             }
                         }else{
@@ -113,37 +118,207 @@ class Hook{
 
     public function processHelp()
     {
-        return "Processado!";
+        $text = "";
 
+        $contador =  count($this->available_commands);
+        $value_array = $this->available_commands;
+        $text .= "DBSnoop is a database monitoring tool. You can check out more about DBSnoop at http://www.dbsnoop.com/. This bot was created so  you can get alerts on the go, and request reports about you server's health. You need to link your Telegram account to a DBSnoop account. You can use /gettingstarted for this.\n\n";
+        $text .= "Other commands that you can use are:\n\n";
+        for ($i=0; $i < $contador; $i++) { 
+        $text .= "/".$value_array[$i]['command'] . " - " . $value_array[$i]['description'] . "\n";
+        }
 
+        return $text;
     }
 
-    public function processTeste_rodolfo($user)
+    public function processGet_servers_error($user)
     {
+            $lang  = new Lang();
+            $lang->setLang($user->getPreferred_language());
 
-        return "Fiz a porra toda!" . $user->getName();
+            $filter = array(
+                'user' => $user->getId()
+            );
+            $server_list = new ServersStatus($filter);
+            $list = [];
+            $array = [];
+            $limit = 3500;
+            $current_text = '';
+            
+            foreach($server_list->toArray() as $server){
+                if($server['status'] == 'R'){
+                    if(!is_array($array[$server['server_name']])) $array[$server['server_name']] = [];
+                        // array_push($array[$server['server_name']], $lang($server['alerts'],true));
+                        array_push($array[$server['server_name']], $server['alerts']);
+                        array_push($array[$server['server_name']], $server['status']);
+                        array_push($array[$server['server_name']], $server['customer_name']);
+                }
+            }
+            foreach ($array as $key2 => $value2) {
 
+            $icon = "🚨";
+            $text = '';
+            $value_alert = str_replace(";", "\n", $value2[0]);
+            $text .= " $icon  $key2 ('".$value2[2]."') \n";
+            $text .= "$value_alert\n\n";
+            
 
-    }
-
-    public function processCache($user){
-
-        $filter = array(
-            'user' => $user->getId()
-        );
-        $server_list = new ServersStatus($filter);
-
-        $list = "";
-
-        foreach($server_list->toArray() as $server){
-            if($server['status'] == 'R'){
-                $list .= " - [". $server['customer_name'] ."]" . $server['server_name'] . " - ". $server['alerts'] . "\n";
+            if(strlen($current_text) + strlen($text) >=$limit){
+                $list[] = $current_text;
+                $current_text = $text;
+            }else{
+                $current_text .= $text;
             }
         }
-        $list2= [];
-        $list2[] = substr($list, 0, 4000);
-        $list2[] = substr($list, 4000, 7000);
-        return $list2;
+        $list[] = $current_text;
 
+        return $list;
     }
+
+
+
+
+    public function processGet_servers_alert($user)
+    {
+                 $filter = array(
+                'user' => $user->getId()
+            );
+            $lang  = new Lang();
+            $lang->setLang($user->getPreferred_language());
+            $server_list = new ServersStatus($filter);
+            $list = [];
+            $array = [];
+            
+            foreach($server_list->toArray() as $server){
+                if($server['status'] == 'Y'){
+                    if(!is_array($array[$server['server_name']])) $array[$server['server_name']] = [];
+                        // array_push($array[$server['server_name']], $lang($server['alerts']));
+                        array_push($array[$server['server_name']], $server['alerts']);
+                        array_push($array[$server['server_name']], $server['status']);
+                        array_push($array[$server['server_name']], $server['customer_name']);
+                }
+            }
+            $current_text = "";            
+            $limit = 3500;
+            foreach ($array as $key2 => $value2) {
+                $text = "";
+                $icon = "⚠";
+                $value_alert = str_replace(";", "\n", $value2[0]);
+                $text .= " $icon  $key2 ('".$value2[2]."') \n";
+                $text .= "$value_alert\n\n";
+
+                if(strlen($current_text) + strlen($text) >=$limit){
+                    $list[] = $current_text;
+                    $current_text = $text;
+                }else{
+                    $current_text .= $text;
+                }
+            }
+            $list[] = $current_text;
+
+            return $list;
+    }
+
+
+    public function processGet_status_maintenance($user)
+    {
+                 $filter = array(
+                'user' => $user->getId()
+            );
+            $lang  = new Lang();
+            $lang->setLang($user->getPreferred_language());
+            $server_list = new ServersStatus($filter);
+            $list = [];
+            $array = [];
+            
+            foreach($server_list->toArray() as $server){
+                if($server['status'] == 'B'){
+                    if(!is_array($array[$server['server_name']])) $array[$server['server_name']] = [];
+                        array_push($array[$server['server_name']], $server['alerts']);
+                        array_push($array[$server['server_name']], $server['status']);
+                        array_push($array[$server['server_name']], $server['customer_name']);
+                }
+            }
+            $current_text = "";            
+            $limit = 3500;
+            foreach ($array as $key2 => $value2) {
+                $text = "";
+                $icon = "🔵";
+                $text .= " $icon  $key2 ('".$value2[2]."') \n";
+                
+                $text .= "  $value2[0]  \n\n\n";
+
+                if(strlen($current_text) + strlen($text) >=$limit){
+                    $list[] = $current_text;
+                    $current_text = $text;
+                }else{
+                    $current_text .= $text;
+                }
+            }
+            $list[] = $current_text;
+
+            return $list;
+    }
+        
+    public function processGet_all_status($user)
+    {
+             $filter = array(
+                'user' => $user->getId()
+            );
+
+            $lang  = new Lang();
+            $lang->setLang($user->getPreferred_language());
+            $server_list = new ServersStatus($filter);
+            $list = [];
+            $array = [];
+            $current_text = "";
+            $limit = 3500;        
+
+            foreach($server_list->toArray() as $server){
+                 if($server['status'] != 'N'){
+                    if(!is_array($array[$server['server_name']])) $array[$server['server_name']] = [];
+                        //array_push($array[$server['server_name']], $lang($server['alerts']));
+                        array_push($array[$server['server_name']], $server['alerts']);
+                        array_push($array[$server['server_name']], $server['status']);
+                        array_push($array[$server['server_name']], $server['customer_name']);
+                }
+            }
+            foreach ($array as $key2 => $value2) {
+                $icon='';
+                $text = "";
+                switch ($value2[1]){
+                    case 'R':
+                        $icon = "🚨";
+                        break;
+                    case 'Y':
+                        $icon = "⚠";
+                        break;
+                    case 'B':
+                        $icon = "🔵";
+                        break;
+                    case 'G':
+                        $icon = "✅";
+                }
+                
+            $value_alert = str_replace(";", "\n", $value2[0]);
+            $text .= " $icon  $key2 ('".$value2[2]."') \n";
+            $text .= "$value_alert \n\n";
+            // if($value2[0] != " " || $value2[0] == NULL){
+            //     $text .= "  $value2[0]  \n\n\n";
+            // }else{
+            //     $text .= " - $value2[0]  \n\n\n";
+            // }
+            
+            if(strlen($current_text) + strlen($text) >=$limit){
+                $list[] = $current_text;
+                $current_text = $text;
+            }else{
+                $current_text .= $text;
+            }
+        }
+        $list[] = $current_text;
+
+        return $list;
+    }
+
 }
